644338fbac84798a4a0d36696d8e6aac4c2c8adb
Livhuwani-t <tovhakalelivhuwanil225@gmail.com>
2023-06-16 07:22:48 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,13 +3,14 @@
 #include <stdio.h>
 
 /**
- * main -Entry point 
+ * main -Entry point
+ *
  * Return: 0 (Success)
  */
 
 
 int main(void)
-{  
+{
 	char low;
 
 	for (low = 'a'; low <= 'z'; low++)
